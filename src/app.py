76a67094a743af525415459a13f7ccacb0f1c4db
gordontsui <tsui.gordon.9@gmail.com>
2021-02-21 00:10:10 -0800

    change post request to url

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, abort, \
     send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
+import urllib.request 
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
@@ -26,14 +27,17 @@ def index():
 
 @app.route('/', methods=['POST'])
 def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    imageURL = request.form["url"]
+    urllib.request.urlretrieve(imageURL, "uploads/local-filename.jpg")
+
+    # uploaded_file = request.files['file']
+    # filename = secure_filename(uploaded_file.filename)
+    # if filename != '':
+    #     file_ext = os.path.splitext(filename)[1]
+    #     if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+    #             file_ext != validate_image(uploaded_file.stream):
+    #         abort(400)
+    #     uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return redirect(url_for('index'))
 
 @app.route('/uploads/<filename>')
